46de26697016e6c29c28de72c657cb4be608e1f1
user <user@10.89.106.30>
2018-09-14 09:56:13 +0800

    exercise 2: comp and mobilecomp class added

---
+++ src/main/java/lab2/ex2/MobileComputer.java
@@ -0,0 +1,16 @@
+package lab2.ex2;
+
+public class MobileComputer extends Computer{
+	private int bat;
+	public MobileComputer() {
+		secret = "MobileComp secret!";
+		bat=5;
+	}
+	public void work() { //override
+		if (bat>0) System.out.println("It's working on my lap. Bat level = "+(bat--));
+		else System.out.println("Running out of battery!");
+	}
+	public void charge() {
+		if (bat<10)bat++;
+	}
+}
